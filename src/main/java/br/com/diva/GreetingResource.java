2f53d0376aaab42056078ba4598261f1302c95a3
divaprates <divasilvaprates@gmail.com>
2023-06-08 15:00:00 -0300

    Creating resource and service

--- src/main/java/br/com/diva/GreetingResource.java
+++ src/main/java/br/com/diva/GreetingResource.java
@@ -1,5 +1,12 @@
 package br.com.diva;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -8,9 +15,35 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    @Inject
+    GreetingService greetingService = new GreetingService();
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public String greeting(String name) {
+        return greetingService.greeting(name);
+    }
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Hello from RESTEasy Reactive!";
     }
+
+    @GET
+    @Path("/users")
+    public Set<User> getUsers() {
+        Set<User> users = Collections.synchronizedSet(new LinkedHashSet<>());
+        users.add(new User(UUID.randomUUID(), "Diva"));
+        users.add(new User(UUID.randomUUID(), "Sophia"));
+        users.add(new User(UUID.randomUUID(), "Bela"));
+        users.add(new User(UUID.randomUUID(), "Lua"));
+
+        return users;
+    }
+
+    record User(UUID id, String name) {
+    }
+
 }
---
+++ src/main/java/br/com/diva/GreetingService.java
@@ -0,0 +1,11 @@
+package br.com.diva;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+    
+    public String greeting(String name) {
+        return "Hello, " + name;
+    }
+}
